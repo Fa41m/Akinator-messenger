c7cd6bbae6e41087515e6b3cbb24e58d6e418618
unknown <price.mikhail@gmail.com>
2022-03-28 00:10:18 +0100

    Added react code - need repo owner to create react environment

---
+++ akinator-react.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+var database = [
+    { "name":"Ironman", "human":true, "youtube":false, "movie":true, "book":true, "female":false},
+    { "name":"Homer Simpson", "human":false, "youtube":false, "movie":true, "book":false, "female":false}
+]
+class Game extends React.Component {
+    render(){
+        return(
+            <h1>Test</h1>
+        )
+    }
+}
