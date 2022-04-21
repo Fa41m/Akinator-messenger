978b5cd61e5acc3cdfdce74303ec7292462ca019
Mikhail Price <price.mikhail@gmail.com>
2022-04-21 19:45:04 +0100

    Fixed prev commit

--- akinator-react/react-side/src/server.js
+++ akinator-react/react-side/src/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.listen(5000)
 
-app.get('/express_backend', (req, res) => { //Line 9
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-  }); //Line 11
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+  });
